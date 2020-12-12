218981de0b465d916949d9bd854ba93ac1070d66
Nerozzz95 <64553939+Nerozzz95@users.noreply.github.com>
2020-12-12 18:27:09 -0500

    Nerozzz95

---
+++ User/accessingdatamysql/SqlQuery.sql
@@ -0,0 +1,12 @@
+﻿package com.olemba;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class USER-LOGIN-SIGN-IN {
+
+	public static void main(String[] args) {
+		SpringApplication.run(USER-LOGIN-SIGN-IN.class, args);
+	}
+}
