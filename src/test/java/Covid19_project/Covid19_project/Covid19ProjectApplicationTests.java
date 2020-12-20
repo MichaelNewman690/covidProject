c278f0ba06a1a5900129dd2d2facd597160e4f57
hanfengliu <70987320+hanfengliu@users.noreply.github.com>
2020-12-20 11:29:05 -0500

    Add files via upload

---
+++ src/test/java/Covid19_project/Covid19_project/Covid19ProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package Covid19_project.Covid19_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Covid19ProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
