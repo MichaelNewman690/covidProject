f1ede98c8db5bbb6bef874119a266d0ee9b01cb0
hanfengliu <70987320+hanfengliu@users.noreply.github.com>
2020-12-21 02:44:42 -0500

    Update covid19TrackerRepository.java

--- src/main/java/Covid19_project/Covid19_project/repository/covid19TrackerRepository.java
+++ src/main/java/Covid19_project/Covid19_project/repository/covid19TrackerRepository.java
@@ -3,5 +3,7 @@ package Covid19_project.Covid19_project.repository;
 import Covid19_project.Covid19_project.model.covid19TrackerModel;
 import org.springframework.data.repository.CrudRepository;
 
+// The repository will retrieve and store all the data. 
+// This will automatically read and save data from PostgreSQL
 public interface covid19TrackerRepository extends CrudRepository<covid19TrackerModel, Integer> {
 }
