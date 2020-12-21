f26e4a87c8d6bf4d89a2049c9b049d22e9fd8f4c
MichaelNewman690 <60640730+MichaelNewman690@users.noreply.github.com>
2020-12-20 21:26:51 -0500

    Add files via upload

--- src/main/java/Covid19_project/Covid19_project/dao/ReviewsDao.java
+++ src/main/java/Covid19_project/Covid19_project/dao/ReviewsDao.java
@@ -1,1 +1,17 @@
+package com.example.covidProject.dao;
 
+import com.example.covidProject.model.Review;
+
+import java.util.List;
+import java.util.UUID;
+
+public interface ReviewsDao {
+    int insertReview(Review review);
+
+    List<Review> selectAllReviews(String country);
+
+    int deleteReview(Review review);
+
+    int updateReview(Review review);
+
+}
